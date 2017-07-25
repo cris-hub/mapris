fbc04fa1cfef692e2613bd94849690d647189597
N1KoLach0 <N1KoLach0@KoLach0>
2017-07-25 01:01:22 -0500

    Version 10.5 NEW vista programaspre

---
+++ web/resources/js/interface/materialcard.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    $('.boton-custom').on('click', function() {
+        $('.card-reveal').slideToggle('slow');
+    });
+
+    $('.card-reveal .close').on('click', function() {
+        $('.card-reveal').slideToggle('slow');
+    });
+});
