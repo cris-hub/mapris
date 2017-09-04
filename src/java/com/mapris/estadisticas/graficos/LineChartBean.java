df1d4afd45c7c305fd301b941d5e61df0ddbf730
kiler1313 <kilerleper@gmail.com>
2017-09-04 01:27:15 -0500

    Implementacion de Aplazamientos full y comienzo de gráficos

--- src/java/com/mapris/estadisticas/graficos/LineChartBean.java
+++ src/java/com/mapris/estadisticas/graficos/LineChartBean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mapris.usuarios.reportes.graficos;
+package com.mapris.estadisticas.graficos;
 
 import java.util.ArrayList;
 import java.util.List;
