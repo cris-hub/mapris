52ea5a82bcb353bd7e2f7f38618da2fa1e92a4b3
kiler1313 <kilerleper@gmail.com>
2017-09-04 02:17:10 -0500

    Intento de gráficos.

--- src/java/com/mapris/estadisticas/graficos/UsuariosEstadisticasController.java
+++ src/java/com/mapris/estadisticas/graficos/UsuariosEstadisticasController.java
@@ -1,9 +1,14 @@
 package com.mapris.estadisticas.graficos;
 
 
+import com.mapris.modelo.dao.UsuarioFacadeLocal;
+import com.mapris.modelo.entitie.Usuario;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 import javax.inject.Named;
@@ -22,7 +27,9 @@ import org.primefaces.model.chart.ChartSeries;
 @Stateless
 public class UsuariosEstadisticasController implements Serializable {
     private BarChartModel barModel;
-    private HorizontalBarChartModel horizontalBarModel;
+    @EJB
+    private UsuarioFacadeLocal ufl;
+    private List<Usuario> usuarios;
 
     public UsuariosEstadisticasController() {
     }
@@ -30,39 +37,53 @@ public class UsuariosEstadisticasController implements Serializable {
     
     @PostConstruct
     public void init() {
-       createBarModel();
-       createHorizontalBarModel();
+        usuarios = ufl.findAll();
+        createBarModel();
+       
+     
     }
  
     public BarChartModel getBarModel() {
         return barModel;
     }
      
-    public HorizontalBarChartModel getHorizontalBarModel() {
-        return horizontalBarModel;
-    }
+    
  
     private BarChartModel initBarModel() {
         BarChartModel model = new BarChartModel();
  
-        ChartSeries boys = new ChartSeries();
-        boys.setLabel("Boys");
-        boys.set("2004", 120);
-        boys.set("2005", 100);
-        boys.set("2006", 44);
-        boys.set("2007", 150);
-        boys.set("2008", 25);
+        List<Usuario> list1 = new ArrayList();
+        List<Usuario> list2 = new ArrayList();
+        List<Usuario> list3 = new ArrayList();
+        List<Usuario> list4 = new ArrayList();
+        
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (usuarios.get(i).getIdEstados().getIdEstados()==1) {
+                list1.add(usuarios.get(i));
+            }else if(usuarios.get(i).getIdEstados().getIdEstados()==2){
+                
+                list2.add(usuarios.get(i));
+            }else if(usuarios.get(i).getIdEstados().getIdEstados()==3){
+                
+                list3.add(usuarios.get(i));
+            }else if(usuarios.get(i).getIdEstados().getIdEstados()==4){
+                
+                list4.add(usuarios.get(i));
+            }
+        }
+        
+        ChartSeries usuarios = new ChartSeries();
+        usuarios.setLabel("Usuarios");
+        usuarios.set("Activos", list1.size());
+        usuarios.set("Bloqueado", list2.size());
+        usuarios.set("Sin Rol", list3.size());
+        usuarios.set("Aplazado", list4.size());
+      
  
-        ChartSeries girls = new ChartSeries();
-        girls.setLabel("Girls");
-        girls.set("2004", 52);
-        girls.set("2005", 60);
-        girls.set("2006", 110);
-        girls.set("2007", 135);
-        girls.set("2008", 120);
+        
  
-        model.addSeries(boys);
-        model.addSeries(girls);
+        model.addSeries(usuarios);
+        
          
         return model;
     }
@@ -72,51 +93,18 @@ public class UsuariosEstadisticasController implements Serializable {
     private void createBarModel() {
         barModel = initBarModel();
         barModel.setAnimate(true);
-        barModel.setTitle("Bar Chart");
+        barModel.setTitle("Usuarios/Estados");
         barModel.setLegendPosition("ne");
          
         Axis xAxis = barModel.getAxis(AxisType.X);
-        xAxis.setLabel("Gender");
+        xAxis.setLabel("Estados");
          
         Axis yAxis = barModel.getAxis(AxisType.Y);
-        yAxis.setLabel("Births");
+        yAxis.setLabel("Número de usuarios");
         yAxis.setMin(0);
-        yAxis.setMax(200);
+        yAxis.setMax(40);
     }
      
-    private void createHorizontalBarModel() {
-        horizontalBarModel = new HorizontalBarChartModel();
- 
-        ChartSeries boys = new ChartSeries();
-        boys.setLabel("Boys");
-        boys.set("2004", 50);
-        boys.set("2005", 96);
-        boys.set("2006", 44);
-        boys.set("2007", 55);
-        boys.set("2008", 25);
- 
-        ChartSeries girls = new ChartSeries();
-        girls.setLabel("Girls");
-        girls.set("2004", 52);
-        girls.set("2005", 60);
-        girls.set("2006", 82);
-        girls.set("2007", 35);
-        girls.set("2008", 120);
- 
-        horizontalBarModel.addSeries(boys);
-        horizontalBarModel.addSeries(girls);
-         
-        horizontalBarModel.setTitle("Horizontal and Stacked");
-        horizontalBarModel.setLegendPosition("e");
-        horizontalBarModel.setStacked(true);
-         
-        Axis xAxis = horizontalBarModel.getAxis(AxisType.X);
-        xAxis.setLabel("Births");
-        xAxis.setMin(0);
-        xAxis.setMax(200);
-         
-        Axis yAxis = horizontalBarModel.getAxis(AxisType.Y);
-        yAxis.setLabel("Gender");        
-    }
+   
  
 }
